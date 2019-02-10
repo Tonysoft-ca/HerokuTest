b03465abd7784222dff4498bccbb5807649b9eff
Tonysoftca <tony@tonysoft.ca>
2019-02-10 15:56:05 -0700

    MIT LICENSE

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const jwt=require('jsonwebtoken');
 const PORT = process.env.PORT || 5000
 const {Pool}=require('pg');
 const pool= new Pool({
